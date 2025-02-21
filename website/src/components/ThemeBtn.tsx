d3692e08151ad0f3cd5d2110a4f5e31be3bd8458
abhinab-choudhury <abhinabchoudhury291@gmail.com>
2025-02-21 21:40:35 +0530

    ui updated

--- website/src/components/ThemeBtn.tsx
+++ website/src/components/ThemeBtn.tsx
@@ -3,17 +3,19 @@
 import * as React from 'react';
 import { useTheme } from 'next-themes';
 
-import { Switch } from '@/components/ui/switch';
+import { Button } from './ui/button';
+import { Moon, Sun } from 'lucide-react';
 
-export function ThemeModeToggle() {
-  const { setTheme } = useTheme();
+export default function ThemeModeToggle() {
+  const { theme ,setTheme } = useTheme();
 
   return (
-    <div className="flex items-center space-x-2">
-      <Switch
-        id="airplane-mode"
-        onClick={() => setTheme((prev) => (prev === 'dark' ? 'light' : 'dark'))}
-      />
-    </div>
+    <Button variant={"ghost"} onClick={() => setTheme((prev) => (prev === 'dark' ? 'light' : 'dark'))} size="icon">
+      { theme === "dark" ? 
+        <Moon className="size-5 text-indigo-400 hidden dark:block" />
+        :
+        <Sun className="size-5 text-indigo-600 dark:hidden" />
+      }
+    </Button>
   );
-}
+};
